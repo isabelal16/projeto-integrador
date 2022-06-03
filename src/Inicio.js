c3b6d3ce72d9e09443ebaadd9c5116ff21b43c3d
isabelal16 <105455967+isabelal16@users.noreply.github.com>
2022-06-03 13:49:02 -0300

    Update Inicio.js

--- src/Inicio.js
+++ src/Inicio.js
@@ -10,7 +10,7 @@ const Inicio = () => {
         <div className='painel'>
             <Menu/>
             <div className='titulo'>
-                <h1>Cuide do seu <br/>melhor amigo</h1> 
+                <h1>Cuide do seu <br/>melhor amigo!</h1> 
                 <p>Nossa clinica é especializada em consulta veterinária, <br/>cuidado com o seu pet e em vacinação.</p> 
             </div>
 
