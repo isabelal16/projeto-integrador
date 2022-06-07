df7b00e2a2e34e4b6b05b4e3459e41c7c8e8d0a6
GiiAraujo <giovanaaraujo2808@gmail.com>
2022-06-07 14:22:27 -0300

    teste

--- src/Inicio.js
+++ src/Inicio.js
@@ -10,7 +10,7 @@ const Inicio = () => {
         <div className='painel'>
             <Menu/>
             <div className='titulo'>
-                <h1 className='painel1'>Cuide do seu <br/>melhor amigo</h1> 
+                <h1 className='painel1'>Cuide do seu <br/>melhor amigo!</h1> 
                 <p className='painel2'>Nossa clinica é especializada em consulta veterinária, <br/>cuidado com o seu pet e em vacinação.</p> 
             </div>
 
