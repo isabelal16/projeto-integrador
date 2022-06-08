cf19b66b18ae830d507c0b2ebe1cbd0e60c7878c
guilherme-hd <105455955+guilherme-hd@users.noreply.github.com>
2022-06-08 15:31:02 -0300

    projeto

--- src/Menu.js
+++ src/Menu.js
@@ -8,15 +8,20 @@ const Menu = ( ) => {
     return (  
 
         <div className="menu">
+
             <div className='logo'>
+
                 <img src='logo.png'></img>
                 <h2>Avocados Bichos</h2>
+            
             </div>
 
             <div className='titulos'>
+
                 <Link className='link' to="/cadastro">Cadastro</Link>
                 <Link className='link' to="/login">Login</Link>
-                <Link className='link' to="/servicos">Serviços</Link>
+                <Link className='link' to="/servicos">Servicos</Link>
+
             </div>
             
         </div>
