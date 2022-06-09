b96ed63ee0f43dd383dbce62601cac53bfdb9547
GiiAraujo <giovanaaraujo2808@gmail.com>
2022-06-09 17:03:37 -0300

    Perfil

--- src/Perfil.js
+++ src/Perfil.js
@@ -15,8 +15,19 @@ const Perfil = () => {
             <h3>Pets</h3>
             <div className='fotopet'> 
                 <img src='gato.png'></img> <br/>
+                     <div className='TextoP'>
+                        <p> Gatos</p>
+                     </div>
                 <img src='doguinho.png'></img>
+                     <div className='TextoP'>
+                         <p> Cachorros</p>
+                     </div>
+                <img src="outros.png"/>
+                    <div className='TextoP'>
+                         <p> Outra Diversidade de animais </p>
+                    </div>
             </div>
+            <div/>
             <div className='add'><img src='add.png'></img></div>
                 <Rodape/>
         </div>
