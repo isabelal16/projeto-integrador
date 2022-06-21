1192cd228ff9b768361ea8a703e942e97db2e320
GiiAraujo <giovanaaraujo2808@gmail.com>
2022-06-21 14:28:57 -0300

    sihadjl

--- src/Admin.js
+++ src/Admin.js
@@ -6,6 +6,30 @@ const Admin = () => {
     return ( 
         <div>
 
+            <h1>Administração</h1>
+
+            <h2>Profissionais</h2>
+
+            <p>Alex</p>
+
+            <p>Samantha</p>
+
+            <img></img>
+
+            <p>Adm</p>
+
+            <p>Agenda</p>
+            
+            <p>Tosador</p>
+
+            <p>Veterinário</p>
+
+            <p>Enfermaria</p>
+
+            <p>Consultas</p>
+
+            <p>Clientes</p>
+
         </div>
      );
 }
