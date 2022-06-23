8fc4d63dde606ef835c8287533f14c29e1a77dde
isabelal16 <105455967+isabelal16@users.noreply.github.com>
2022-06-23 17:03:21 -0300

    rsrs

--- src/PagServicos.js
+++ src/PagServicos.js
@@ -4,6 +4,8 @@ import { Routes, Route, Link } from "react-router-dom";
 
 const PagServicos = () => {
 
+    
+
     const [count, setCount] = useState(0);
 
     const servicos = [
@@ -16,29 +18,31 @@ const PagServicos = () => {
               cãozinho e evitar problemas de saúde tanto do animal, quanto 
                do seu dono. Pulgas, carrapatos e todo tipo de bactéria ou 
                doença de pelos e pele podem ser facilmente evitadas para que o 
-                dono não precise gastar mais tendo que tratá-las.`
+                dono não precise gastar mais tendo que tratá-las.`,
+            img:"Banho.png"
         },
 
         {
             servico: "Vacina",
-            descricao: "Neste serviço damos vacina"
+            descricao: "É só uma picadinha!. As vacinas são aplicadas para dar ao seu pet maior tempo de vida, auxiliar na saúde do seu pet e até mesmo na sua, te previnindo de algumas Zoonozes.",
+            img:"Banho.png"
         },
 
         {
-            servico: "Tosa",
-            descricao: "Neste serviço damos tosa"
+            servico: "Consulta",
+            descricao: "Aqui neste espaço, seu animalzinho será atendido particularmente por um profissional da clínica. Ele realizará procedimentos que ajudaram com sua saúde, e indicará tratamentos necessários para um bem estar maior ao seu animalzinho. Aqui também será onde seu pet será encaminhado para cirurgias caso necessário. Os atendimentos são 24horas.",
+            img:"Banho.png"
         }
        
     ]
     require("./PagServicos.css");
 
     return ( 
-
         
         <div className='pagservico'>
             <Menu/>
         <h1>{servicos[0].servico}</h1>
-        <div className='textimg'><img src='Banho.png'/>
+        <div className='textimg'><img src={servicos[0].img}/>
         <p>{servicos[0].descricao}</p></div>
         <h1>Profissionais</h1>
         <div className='profissionais'>
