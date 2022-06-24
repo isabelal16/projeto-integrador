59fd17dab3f9c85fc63fb79eda7f61002bca10b6
maryasouz <105455969+maryasouz@users.noreply.github.com>
2022-06-24 13:57:40 -0300

    Update Perfil.js

--- src/Perfil.js
+++ src/Perfil.js
@@ -67,7 +67,6 @@ const Perfil = (props) => {
             <div className='fundo'></div>
             <div><img className='fotoU' src="perfil.png"/></div>
             <h1 className='textosPer'>Sara Silva</h1>
-            <h3>Pets</h3>
             <div className='petsCadastro'>
 
                 <br/>
@@ -83,6 +82,7 @@ const Perfil = (props) => {
 
                                     <hr/>
                                     <p>Nome: {u.nome}</p>
+                                    <p>Espécie: {u.especie}</p>
                                     <p>Raça: {u.raça}</p>
                                     <p>Porte: {u.porte}</p>                                                               
                                     <hr/>
@@ -94,12 +94,39 @@ const Perfil = (props) => {
                 }
 
             </div>
-            <div className='fotopet'/> 
-                <img src='gato.png' onClick={AbrirModal}></img> <br/>                     
-                <img src='doguinho.png'></img>
-                <img src="outros.png"/>
-                    <div className='TextoP'/>                   
-                    <div className='add'><img src='add.png' ></img></div>
+            <div className='alinhamento'>
+                    <div className='Pets'>
+                        <h3>Pets</h3>
+                        <div className='fotopet'> 
+                            <img src='gato.png' onClick={AbrirModal}></img> <br/>
+                            
+                            <div className='TextoP'>
+                                <p> Gatos</p>
+                            </div>
+
+                            <img src='doguinho.png'></img>
+                            <div className='TextoP'>
+                                <p> Cachorros</p>
+                            </div>
+
+                            <img src="outros.png"/>
+                                <div className='TextoP'>
+                                    <p> Outra Diversidade de animais </p>
+                                </div>
+                        </div>
+                    </div>
+               
+
+                <div className='Pets'>
+                    <h3>Agenda</h3>
+                  <p>Pet:</p>  
+                  <p>Data:</p>
+                  <p>Horário:</p>
+                  <p>Serviço:</p>
+                        
+                        
+                </div>
+            </div>
             
 
             <div className='add' onClick={OpenModal}><img src='add.png'></img></div>
