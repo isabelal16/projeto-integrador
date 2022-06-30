92168474856d505cf69ce07936a1d248e9cd4df8
guilherme-hd <105455955+guilherme-hd@users.noreply.github.com>
2022-06-30 16:49:48 -0300

    oi

--- src/Profissionais.js
+++ src/Profissionais.js
@@ -12,35 +12,33 @@ const Profissionais = (props) => {
     const profissionais = [
 
         
-
+        
         {
             nome: "Alex Silva",
             descricao: `Alex é especializado em diversos tipos de banhos e tosquias, trabalha na área há mais de 10 anos e é apaixonado 
             por animais silvestres, recentemente está cursando seu primeiro semestre em Veterinária na Univesp, e com todo carinho e paciência,
              é responsavel pela área de banho e tosa da nossa clínica.`,
-            especialidade: "Banho e Tosa",
             imgprof: "alex.png"
         },
 
         {
-            nome: "Ana Souza",
-            descricao: "Alex, juntamente com uma equipe de 4 integrantes, são responsáveis pelo Banho e a Tosa do seu querido amigo.",
-            especialidade: "Banho e Tosa",
+            nome: "Carlos Ferreira",
+            descricao:` Carlos esta no segundo semestre da faculdade de medicina veterinaria
+            ele sempra amou os pets e tem uma Familia que atua nessa area a mais de 3 geração e por isso  sempre amo os pets que a Familia cuidava `,
             imgprof: "profissional.png"
         },
        
         {
-            nome: "Carlos Ferreira",
-            descricao: "Alex, juntamente com uma equipe de 4 integrantes, são responsáveis pelo Banho e a Tosa do seu querido amigo.",
-            especialidade: "Banho e Tosa",
+            nome: "Anna souza ",
+            descricao: "Anna esta no utimo ano da facudade de medicina veterinaria ela como uma ",
             imgprof: "profissional1.png"
         },
        
-        {
-            nome: "Maria Fernanda",
+            
+        { 
+            nome: "Maria Rita",
             descricao: "Alex, juntamente com uma equipe de 4 integrantes, são responsáveis pelo Banho e a Tosa do seu querido amigo.",
-            especialidade: "Banho e Tosa",
-            imgprof: "profissional1.png"
+            imgprof: "ana.png"
         } 
        
     ]
@@ -54,31 +52,33 @@ const Profissionais = (props) => {
             <img src='veterinary.gif' className='vet'/>
             </div>
             <div className='profsimg'>
+
                 <div className='profs'>
                     <img className='profissional' src={profissionais [1].imgprof}/>
                     <h1>{profissionais[0].nome}</h1>
                     <p>{profissionais[0].descricao}</p>
-                    <p>{profissionais [0].especialidade}</p>
+
                 </div>
+
                 <div className='profs'>
                 <img className='profissional' src={profissionais [1].imgprof}/>
                 <h1>{profissionais[1].nome}</h1>
                 <p>{profissionais[1].descricao}</p>
-                <p>{profissionais [1].especialidade}</p>
                 </div>
+
                 <div className='profs'>
                     <img className='profissional' src={profissionais [2].imgprof}/>
                     <h1>{profissionais[2].nome}</h1>
                     <p>{profissionais[2].descricao}</p>
-                    <p>{profissionais [2].especialidade}</p>
+ 
                 </div>
+
                 <div className='profs'>
                     <img className='profissional' src={profissionais [3].imgprof}/>
                     <h1>{profissionais[3].nome}</h1>
                     <p>{profissionais[3].descricao}</p>
-                    <p>{profissionais [3].especialidade}</p>
                 </div>
-                </div>
+            </div>
            
                 
             {/* <div className='textop'>
